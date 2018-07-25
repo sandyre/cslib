d001ebf1995ec157f6ac055b54b49809164fd384
Alexandr Borzykh <aleksandr.borzyh@gs-labs.ru>
2018-07-25 14:48:22 +0300

    data_structure/dynamic_array: bunch of redesign and work

--- cslib/data_structure/dynamic_array.hpp
+++ cslib/data_structure/dynamic_array.hpp
@@ -1,6 +1,7 @@
 #ifndef CSLIB_DATA_STRUCTURE_DYNAMIC_ARRAY_HPP
 #define CSLIB_DATA_STRUCTURE_DYNAMIC_ARRAY_HPP
 
+#include <cmath>
 #include <stdexcept>
 
 namespace cslib {
@@ -16,31 +17,85 @@ namespace data_structure
 	 *
 	 * Space complexity: O(n)
 	 *
-	 * TODO: push_back(), push_front(), insert(), remove()
+	 * TODO: insert(), remove()
 	 */
 
 	template < typename ValueT >
 	class dynamic_array
 	{
-		using PassingValueT = typename std::conditional<std::is_fundamental<ValueT>::value, ValueT, const ValueT&>::type;
+	public:
+		using value_type			= ValueT;
+		using pointer				= ValueT*;
+		using const_pointer			= const ValueT*;
+		using reference				= ValueT&;
+		using const_reference		= const ValueT&;
+		using iterator				= pointer;
+		using const_iterator		= const_pointer;
+		using size_type				= size_t;
 
 	private:
-		ValueT*		_storage;
-		size_t		_size;
+		using PassingValueT = typename std::conditional<std::is_fundamental<ValueT>::value, ValueT, const ValueT&>::type;
+		using StorageT = void*;
+
+	private:
+		StorageT		_storage;
+		size_type		_capacity;
+		size_type		_size;
 
 	public:
 		dynamic_array()
 			:	_storage(std::nullptr),
+				_capacity(),
 				_size()
 		{ }
 
+		~dynamic_array()
+		{ delete _storage; }
+
 		explicit dynamic_array(size_t initialSize)
 			:	_storage(new ValueT[initialSize]),
+				_capacity(initialSize),
 				_size(initialSize)
 		{ }
 
-		ValueT& at(size_t index)
-		{ return _storage[index]; }
+		void push_back(PassingValueT value)
+		{
+			ensure_capacity(_size + 1);
+			new(_storage[size() * sizeof(ValueT)]) ValueT(value);
+		}
+
+		void pop_back()
+		{
+			reinterpret_cast<pointer>(_storage[size() - 1 * sizeof(ValueT)])->~ValueT();
+			--_size;
+		}
+
+		reference at(size_type index)			{ return _storage[index]; }
+		reference operator[](size_type index)	{ return at(index); }
+
+		iterator begin() const			{ return _storage; }
+		iterator end() const			{ return _storage + size(); }
+		const_iterator cbegin() const	{ return begin(); }
+		const_iterator cend() const		{ return end(); }
+
+		void clear()			{ _size = 0; }
+		size_type size() const	{ return _size; }
+
+	private:
+		void ensure_capacity(size_t newSize)
+		{
+			if (newSize >= _capacity)
+			{
+				const size_type newCapacity = _capacity * static_cast<size_type>(std::ceil(std::log2(newSize / _capacity)));
+				StorageT newStorage = ::operator new(newCapacity);
+				std::move(_storage, _storage + _size * sizeof(ValueT), newStorage);
+				delete [] _storage;
+
+				_storage = newStorage;
+				_capacity = newCapacity;
+				_size = newSize;
+			}
+		}
 	};
 
 }}
